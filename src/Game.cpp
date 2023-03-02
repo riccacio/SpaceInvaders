bfb47b7752f8bc82b6c9a452eb70e065446e1bd2
jacopodelmoro <jacopodm2002@gmail.com>
2023-03-02 09:58:35 +0100

    some errors in cmake files fixed

--- src/Game.cpp
+++ src/Game.cpp
@@ -394,7 +394,6 @@ void Game::render() {
 void Game::createShip() {
     map.createShip();
     ship = map.getShip();
-    ship->setTime();
 }
 
 void Game::centerItem(Sprite& sprite, float height){
