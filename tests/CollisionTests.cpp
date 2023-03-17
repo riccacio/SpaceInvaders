dd27b7e8bbdd0fb53ff045c4b6623f4f21180bfc
riccacio <riccardo.pacini@stud.unifi.it>
2023-03-17 11:08:03 +0100

    modified test code

--- tests/CollisionTests.cpp
+++ tests/CollisionTests.cpp
@@ -6,12 +6,11 @@ TEST(Game, checkDeadAliens) {
     std::unique_ptr<Game> game(new Game);
     int initialAlienCount = game->getAliens()->size();
 
-    sf::Texture texShot1, texShot2;
-    texShot1.loadFromFile("sprite/ship_shot.png");
-    texShot2.loadFromFile("sprite/ship_shot.png");
+    sf::Texture texShot;
+    texShot.loadFromFile("sprite/ship_shot.png");
 
     sf::Vector2f bulletPosition = (*game->getAliens())[35]->getPositionA();
-    game->getShip()->getBullets()->emplace_back(texShot1, texShot2, bulletPosition);
+    game->getShip()->getBullets()->emplace_back(texShot, texShot, bulletPosition);
 
     game->checkDeadAliens();
 
